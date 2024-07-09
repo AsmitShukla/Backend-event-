fd53601e39f9106b48e254e5ad9e7888ce0bcced
AsmitShukla <shuklaasmit40@gmail.com>
2024-07-09 13:26:51 +0530

    CHANGED PORT

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 const app = express();
@@ -22,6 +21,6 @@ mongoose
 const eventRouter = require("./routes/events");
 app.use("/api/events", eventRouter);
 //creating our server on port 5000
-app.listen(PORT, () => {
+app.listen(5000, () => {
   console.log(`Server is running on port: 5000`);
 });
